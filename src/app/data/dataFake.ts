0db90a1695256a0779e93a413b7537184a6f0c9e
ArthurCovelo <arthurcovelo@live.com>
2024-02-07 11:09:57 -0300

    Created Pages e Componentes

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Intel quer assumir lugar da AMD no próximo Xbox",
+    "description": "A Intel está conversando com a Microsoft para trabalharem em conjunto no próximo console Xbox, tirando a AMD da jogada, confira",
+    "photoCover":"https://www.adrenaline.com.br/wp-content/plugins/seox-image-magick/imagick_convert.php?width=910&height=568&format=.jpg&quality=91&imagick=uploads.adrenaline.com.br/2023/12/Intel-chip-912x569.jpg"
+  },
+  {
+    "id":"2",
+    "title": "SoC Qualcomm X Elite ARM chega em 2024 com nova versão do Windows",
+    "description": "Novo chip ARM da Qualcoom deve chegar em julho, juntamente ao Windows 11 24H2",
+    "photoCover":"https://www.adrenaline.com.br/wp-content/plugins/seox-image-magick/imagick_convert.php?width=910&height=568&format=.jpg&quality=91&imagick=uploads.adrenaline.com.br/2024/02/SoC-Qualcomm-X-Elite-ARM-chega-em-2024-com-nova-versao-do-Windows-912x569.jpg"
+  },
+  {
+    "id":"3",
+    "title": "CPU AMD Threadripper PRO 7995WX custa mais de R$ 80.000, o mais caro do Brasil",
+    "description": "A CPU AMD Threadripper PRO 7995WX chegou ao Brasil, mas seu valor pode não agradar em nada os bolsos do povo",
+    "photoCover":"https://www.adrenaline.com.br/wp-content/plugins/seox-image-magick/imagick_convert.php?width=910&height=568&format=.jpg&quality=91&imagick=uploads.adrenaline.com.br/2024/02/AMD-Threadripper-PRO-7995WX-CAPA-912x569.jpg"
+  },
+  {
+    "id":"4",
+    "title": "Placas-mãe Z790 da MSI ganham suporte para 256 GB de RAM DDR5",
+    "description": "Atualização dos modelos Z790 pode ser baixado diretamente no site da MSI, mas ainda está em fase beta",
+    "photoCover":"https://www.adrenaline.com.br/wp-content/plugins/seox-image-magick/imagick_convert.php?width=910&height=568&format=.jpg&quality=91&imagick=uploads.adrenaline.com.br/2024/02/Placas-mae-Z790-MSI-suporte-256-GB-de-RAM-DDR5-912x569.jpg"
+  }
+]
